31cfd887117b3fcb2bf86affcdfabcfb3b0dc8d5
motherflanker <geth.jhjgh@mail.ru>
2023-03-23 02:07:02 +0300

    cart reducer full functionality + utils

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import filter from "./filter/slice";
 import guitar from "./guitar/slice"
+import cart from "./cart/slice"
 
 export const store = configureStore({
   reducer: {
     filter,
     guitar,
+    cart
   },
 });
 
